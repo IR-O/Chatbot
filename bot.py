f13f2fe5859b6800b663574ffddaf89fd38b79da
STD DEEPANSHU <108056384+STD-DIPANSHU@users.noreply.github.com>
2026-03-14 21:02:36 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.enums import ChatAction
+from config import API_ID, API_HASH, BOT_TOKEN
+from iro import get_iro_reply
+
+app = Client(
+    "IR-O_Chatbot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+@app.on_message(filters.text & ~filters.command)
+async def chat_handler(client, message):
+    await client.send_chat_action(message.chat.id, ChatAction.TYPING)
+    reply = get_ai_reply(message.text)
+    await message.reply_text(reply)
+
+print("🤖 Desi Chatbot running...")
+app.run()
